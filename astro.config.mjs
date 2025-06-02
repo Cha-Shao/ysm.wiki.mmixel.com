4d0e5620d435e5e1d87dbdeb7caf90a79d456381
ChaShao <yrchashao@outlook.com>
2025-06-02 18:54:00 +0800

    english

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,7 @@ export default defineConfig({
         starlight({
             title: {
                 "zh-CN": "是，史蒂夫模型文档",
-                en: "Yes Steve Model Wiki",
+                'en-US': "Yes Steve Model Wiki",
             },
             social: [{
                 icon: "github",
@@ -31,79 +31,237 @@ export default defineConfig({
                     label: "简体中文",
                     lang: "zh-CN",
                 },
-                'zh-ai': {
-                    label: "AI 中文",
-                    lang: "zh-AI",
+                'en-us': {
+                    label: "English",
+                    lang: "en-US",
                 }
             },
             sidebar: [
                 {
                     label: "简介",
+                    translations: { 'en-US': "Introduction", },
                     slug: "intro"
                 },
                 {
                     label: "新手入门",
+                    translations: { 'en-US': "Getting Started", },
                     items: [
                         // Each item here is one entry in the navigation menu.
-                        { label: "安装使用", slug: "getting-start/install" },
-                        { label: "常见问题解答", slug: "getting-start/faq" },
-                        { label: "模型格式说明", slug: "getting-start/format" },
-                        { label: "配置文件说明", slug: "getting-start/config" },
-                        { label: "相关指令", slug: "getting-start/commands" },
-                        { label: "Freesia 插件", slug: "getting-start/freesia-plugin" },
+                        {
+                            label: "安装使用",
+                            translations: { 'en-US': "Installation", },
+                            slug: "getting-start/install"
+                        },
+                        {
+                            label: "常见问题解答",
+                            translations: { 'en-US': "FAQ", },
+                            slug: "getting-start/faq"
+                        },
+                        {
+                            label: "模型格式说明",
+                            translations: { 'en-US': "Model Format", },
+                            slug: "getting-start/format"
+                        },
+                        {
+                            label: "配置文件说明",
+                            translations: { 'en-US': "Configuration", },
+                            slug: "getting-start/config"
+                        },
+                        {
+                            label: "相关指令",
+                            translations: { 'en-US': "Commands", },
+                            slug: "getting-start/commands"
+                        },
+                        {
+                            label: "Freesia 插件",
+                            translations: { 'en-US': "Freesia Plugin", },
+                            slug: "getting-start/freesia-plugin"
+                        },
                     ],
                 },
                 {
                     label: "模型制作",
+                    translations: { 'en-US': "Modeling", },
                     items: [
                         // Each item here is one entry in the navigation menu.
-                        { label: "准备工具", slug: "modeling/prepare" },
-                        { label: "使用插件制作", slug: "modeling/blockbench-plugin" },
-                        { label: "视频教程", slug: "modeling/video-tutorial" },
-                        { label: "项目结构", slug: "modeling/struct" },
-                        { label: "模型制作", slug: "modeling/model" },
-                        { label: "纹理制作", slug: "modeling/texture" },
-                        { label: "PBR贴图", slug: "modeling/pbr-texture" },
-                        { label: "音频播放", slug: "modeling/sound" },
-                        { label: "自定义轮盘", slug: "modeling/wheel" },
-                        { label: "动画控制器", slug: "modeling/controller" },
+                        {
+                            label: "准备工具",
+                            translations: { 'en-US': "Preparing Tools", },
+                            slug: "modeling/prepare"
+                        },
+                        {
+                            label: "使用插件制作",
+                            translations: { 'en-US': "Using Blockbench Plugin", },
+                            slug: "modeling/blockbench-plugin"
+                        },
+                        {
+                            label: "视频教程",
+                            translations: { 'en-US': "Video Tutorial", },
+                            slug: "modeling/video-tutorial"
+                        },
+                        {
+                            label: "项目结构",
+                            translations: { 'en-US': "Project Structure", },
+                            slug: "modeling/struct"
+                        },
+                        {
+                            label: "模型制作",
+                            translations: { 'en-US': "Model Creation", },
+                            slug: "modeling/model"
+                        },
+                        {
+                            label: "纹理制作",
+                            translations: { 'en-US': "Texture Creation", },
+                            slug: "modeling/texture"
+                        },
+                        {
+                            label: "PBR贴图",
+                            translations: { 'en-US': "PBR Texture", },
+                            slug: "modeling/pbr-texture"
+                        },
+                        {
+                            label: "音频播放",
+                            translations: { 'en-US': "Sound Playback", },
+                            slug: "modeling/sound"
+                        },
+                        {
+                            label: "自定义轮盘",
+                            translations: { 'en-US': "Custom Wheel", },
+                            slug: "modeling/wheel"
+                        },
+                        {
+                            label: "动画控制器",
+                            translations: { 'en-US': "Animation Controller", },
+                            slug: "modeling/controller"
+                        },
                     ],
                 },
                 {
                     label: "动画制作",
+                    translations: { 'en-US': "Animation", },
                     items: [
-                        { label: "动画概述", slug: "animation/intro" },
-                        { label: "玩家主动画", slug: "animation/main" },
-                        { label: "手部条件动画", slug: "animation/arm" },
-                        { label: "并行动画", slug: "animation/parallel" },
-                        { label: "护甲动画", slug: "animation/armor" },
-                        { label: "骑乘动画", slug: "animation/ride" },
-                        { label: "轮盘动画", slug: "animation/emote" },
-                        { label: "箭矢动画", slug: "animation/arrow" },
-                        { label: "选择界面动画", slug: "animation/gui" },
-                        { label: "永恒枪械工坊动画", slug: "animation/tacz" },
-                        { label: "马术动画", slug: "animation/swem" },
-                        { label: "跑酷动画", slug: "animation/parcool" },
-                        { label: "CarryOn动画", slug: "animation/carry-on" },
-                        { label: "拔刀剑动画", slug: "animation/slashblade" },
-                        { label: "女仆动画", slug: "animation/tlm" },
-                        { label: "更真实的第一人称", slug: "animation/first-person" },
+                        {
+                            label: "动画概述",
+                            translations: { 'en-US': "Animation Overview", },
+                            slug: "animation/intro"
+                        },
+                        {
+                            label: "玩家主动画",
+                            translations: { 'en-US': "Player Main Animation", },
+                            slug: "animation/main"
+                        },
+                        {
+                            label: "手部条件动画",
+                            translations: { 'en-US': "Hand Conditional Animation", },
+                            slug: "animation/arm"
+                        },
+                        {
+                            label: "并行动画",
+                            translations: { 'en-US': "Parallel Animation", },
+                            slug: "animation/parallel"
+                        },
+                        {
+                            label: "护甲动画",
+                            translations: { 'en-US': "Armor Animation", },
+                            slug: "animation/armor"
+                        },
+                        {
+                            label: "骑乘动画",
+                            translations: { 'en-US': "Ride Animation", },
+                            slug: "animation/ride"
+                        },
+                        {
+                            label: "轮盘动画",
+                            translations: { 'en-US': "Wheel Animation", },
+                            slug: "animation/emote"
+                        },
+                        {
+                            label: "箭矢动画",
+                            translations: { 'en-US': "Arrow Animation", },
+                            slug: "animation/arrow"
+                        },
+                        {
+                            label: "选择界面动画",
+                            translations: { 'en-US': "GUI Animation", },
+                            slug: "animation/gui"
+                        },
+                        {
+                            label: "永恒枪械工坊动画",
+                            translations: { 'en-US': "Eternal Gunworks Animation", },
+                            slug: "animation/tacz"
+                        },
+                        {
+                            label: "马术动画",
+                            translations: { 'en-US': "Equestrian Animation", },
+                            slug: "animation/swem"
+                        },
+                        {
+                            label: "跑酷动画",
+                            translations: { 'en-US': "Parkour Animation", },
+                            slug: "animation/parcool"
+                        },
+                        {
+                            label: "CarryOn动画",
+                            translations: { 'en-US': "CarryOn Animation", },
+                            slug: "animation/carry-on"
+                        },
+                        {
+                            label: "拔刀剑动画",
+                            translations: { 'en-US': "SlashBlade Animation", },
+                            slug: "animation/slashblade"
+                        },
+                        {
+                            label: "女仆动画",
+                            translations: { 'en-US': "TLM Maid Animation", },
+                            slug: "animation/tlm"
+                        },
+                        {
+                            label: "更真实的第一人称",
+                            translations: { 'en-US': "More Realistic First Person", },
+                            slug: "animation/first-person"
+                        },
                     ]
                 },
                 {
                     label: "molang",
+                    translations: { 'en-US': "Molang", },
                     items: [
-                        { label: "Molang概述", slug: "molang/intro" },
-                        { label: "变量", slug: "molang/var" },
-                        { label: "常用 molang 集合", slug: "molang/common" },
-                        { label: "molang 参考表", slug: "molang/ref" },
+                        {
+                            label: "Molang概述",
+                            translations: { 'en-US': "Molang Overview", },
+                            slug: "molang/intro"
+                        },
+                        {
+                            label: "变量",
+                            translations: { 'en-US': "Variables", },
+                            slug: "molang/var"
+                        },
+                        {
+                            label: "常用 molang 集合",
+                            translations: { 'en-US': "Common Molang Collections", },
+                            slug: "molang/common"
+                        },
+                        {
+                            label: "molang 参考表",
+                            translations: { 'en-US': "Molang Reference", },
+                            slug: "molang/ref"
+                        },
                     ]
                 },
                 {
                     label: "模型转换",
+                    translations: { 'en-US': "Model Conversion", },
                     items: [
-                        { label: "中国版YSM组件", slug: "trans/netease" },
-                        { label: "女仆模组", slug: "trans/maid" },
+                        {
+                            label: "中国版YSM组件",
+                            translations: { 'en-US': "Chinese YSM Components", },
+                            slug: "trans/netease"
+                        },
+                        {
+                            label: "女仆模组",
+                            translations: { 'en-US': "TLM Maid Mod", },
+                            slug: "trans/maid"
+                        },
                     ]
                 },
                 // {
